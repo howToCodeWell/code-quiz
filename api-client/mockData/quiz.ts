047652506021f0f74f04b0c0d9521b64c6d329b1
pfwd <hello@websomatic.co.uk>
2022-09-14 22:33:23 +0100

    [38] Added two HTML questions and answers to the API Fixes #38

--- api-client/mockData/quiz.ts
+++ api-client/mockData/quiz.ts
@@ -1,21 +1,25 @@
-import { QuizDTO } from "../api-models"
+import {QuizDTO} from "../api-models"
+import HTMLQuestions from "./htmlQuestions"
 
 const quiz: QuizDTO[] = [
-  {
-    id: "1",
-    title: "HTML Quiz",
-    slug: 'html-quiz',
-  },
-  {
-    id: "2",
-    title: "JavaScript Quiz",
-    slug: 'javascript-quiz',
-  },
-  {
-    id: "3",
-    title: "CSS Quiz",
-    slug: 'css-quiz',
-  },
+    {
+        id: "1",
+        title: "HTML Quiz",
+        slug: 'html-quiz',
+        questions: HTMLQuestions
+    },
+    {
+        id: "2",
+        title: "JavaScript Quiz",
+        slug: 'javascript-quiz',
+        questions: []
+    },
+    {
+        id: "3",
+        title: "CSS Quiz",
+        slug: 'css-quiz',
+        questions: []
+    },
 
 ]
 
