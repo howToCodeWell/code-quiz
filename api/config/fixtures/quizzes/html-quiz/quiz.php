349fceec53d11f6a1817e9125a28b22fea9f281e
pfwd <hello@websomatic.co.uk>
2022-09-18 18:09:48 +0100

    [45] Adding second HTML quiz question

--- api/config/fixtures/quizzes/html-quiz/quiz.php
+++ api/config/fixtures/quizzes/html-quiz/quiz.php
@@ -1,6 +1,7 @@
 <?php
 
 $question1 = require_once 'questions/question_1.php';
+$question2 = require_once 'questions/question_2.php';
 
 /**
  * @var array{title:string, slug: string, questions: array{array{content:string, quiz: string, answers: array{array{content: string, is_correct: boolean, display_order: integer}} }}} $quiz
@@ -9,7 +10,8 @@ $quiz = [
     'title' => 'HTML Quiz',
     'slug' => 'html-quiz',
     'questions' => [
-        $question1[0]
+        $question1[0],
+        $question2[0]
     ]
 ];
 
