f8b58fc09d799affe335f970feb8e44f730586f6
pfwd <hello@websomatic.co.uk>
2022-09-25 17:41:00 +0100

    #83 Adding entity tests

---
+++ api/tests/unit/Entity/AnswerTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Tests\unit\Entity;
+
+use App\Entity\Answer;
+use App\Entity\Question;
+use PHPUnit\Framework\TestCase;
+
+class AnswerTest extends TestCase
+{
+    public function testDefault()
+    {
+        $entity = new Answer();
+        self::assertEmpty($entity->getContent());
+        self::assertNull($entity->getQuestion());
+        self::assertNull($entity->getId());
+        self::assertNull($entity->isIsCorrect());
+        self::assertNull($entity->getDisplayOrder());
+    }
+
+    public function testSettingContent()
+    {
+        $value = 'This is the answer';
+        $entity = new Answer();
+        $entity->setContent($value);
+        self::assertSame($value, $entity->getContent());
+    }
+
+    public function testSettingDisplayOrder()
+    {
+        $value = 1;
+        $entity = new Answer();
+        $entity->setDisplayOrder($value);
+        self::assertSame($value, $entity->getDisplayOrder());
+    }
+
+    public function testSettingIsCorrect()
+    {
+        $value = true;
+        $entity = new Answer();
+        $entity->setIsCorrect($value);
+        self::assertTrue($entity->isIsCorrect());
+    }
+
+    public function testSettingQuestion()
+    {
+        $value = new Question();
+        $value->setContent('This is the question');
+        $entity = new Answer();
+        $entity->setQuestion($value);
+        self::assertSame('This is the question', $entity->getQuestion()->getContent());
+    }
+}
---
+++ api/tests/unit/Entity/QuestionTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Tests\unit\Entity;
+
+use App\Entity\Answer;
+use App\Entity\Question;
+use App\Entity\Quiz;
+use PHPUnit\Framework\TestCase;
+
+class QuestionTest extends TestCase
+{
+    public function testDefault()
+    {
+        $entity = new Question();
+        self::assertEmpty($entity->getId());
+        self::assertNull($entity->getContent());
+        self::assertEmpty($entity->getAnswers());
+        self::assertNull($entity->getQuiz());
+    }
+
+    public function testSettingContent()
+    {
+        $value = 'This is the title';
+        $entity = new Question();
+        $entity->setContent($value);
+        self::assertSame($value, $entity->getContent());
+    }
+
+    public function testSettingQuiz()
+    {
+        $quizMock = $this->createMock(Quiz::class);
+        $quizMock->expects(self::once())->method('getTitle')->willReturn('quiz title');
+        $entity = new Question();
+        $entity->setQuiz($quizMock);
+        self::assertSame('quiz title', $quizMock->getTitle());
+    }
+
+    public function testRemoveAnswer()
+    {
+        $question = new Question();
+
+        $answerMock = $this->createMock(Answer::class);
+        $answerMock->expects(self::once())->method('getQuestion')->willReturn($question);
+
+        $question->addAnswer($answerMock);
+        $question->removeAnswer($answerMock);
+
+        self::assertCount(0, $question->getAnswers());
+    }
+}
