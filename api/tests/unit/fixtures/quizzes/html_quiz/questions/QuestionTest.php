be013a82d42717b2ec152631d1739f3321555a1f
pfwd <hello@websomatic.co.uk>
2022-09-25 18:29:19 +0100

    #85 Adding fixtures to unit tests

---
+++ api/tests/unit/fixtures/quizzes/html_quiz/questions/QuestionTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Tests\unit\fixtures\quizzes\html_quiz;
+
+use PHPUnit\Framework\TestCase;
+
+class QuizTest extends TestCase
+{
+    public function testDefault()
+    {
+        $quiz = require_once dirname(__DIR__)  .'/../../../../config/fixtures/quizzes/html-quiz/quiz.php';
+
+        self::assertIsArray($quiz);
+    }
+
+}
