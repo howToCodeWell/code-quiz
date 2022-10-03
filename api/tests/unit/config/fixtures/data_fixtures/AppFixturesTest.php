95c31f2e9d96516f37ffe94dd35f962392636102
metinbaris <hizmetparki@gmail.com>
2022-10-03 22:03:44 +0300

    #90 Test Create Quiz
    Add entityManager
    Change test name as KernelTestCase
    Update static assert calling for $this-> to self::

--- api/tests/unit/config/fixtures/data_fixtures/AppFixturesTest.php
+++ api/tests/unit/config/fixtures/data_fixtures/AppFixturesTest.php
@@ -5,10 +5,24 @@ declare(strict_types=1);
 namespace App\Tests\unit\config\fixtures\data_fixtures;
 
 use App\DataFixtures\AppFixtures;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-final class AppFixturesTest extends TestCase
+final class AppFixturesTest extends KernelTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function invokeMethod(&$object, $methodName, array $parameters = array())
     {
         $reflection = new \ReflectionClass(get_class($object));
@@ -24,7 +38,7 @@ final class AppFixturesTest extends TestCase
         
         $result = $this->invokeMethod($class, 'getDataSets');
 
-        $this->assertIsArray($result);
+        self::assertIsArray($result);
     }
 
     public function testGetFilePathsMethod()
@@ -33,6 +47,22 @@ final class AppFixturesTest extends TestCase
         
         $result = $this->invokeMethod($class, 'getFilePaths');
 
-        $this->assertIsArray($result);
+        self::assertIsArray($result);
+    }
+
+    public function testCreateQuiz()
+    {
+        $class = new AppFixtures;
+
+        $data = [
+            'title' => 'Test Quiz Title',
+            'slug' => 'test-quiz-title'
+        ];
+
+        $result = $this->invokeMethod($class, 'CreateQuiz', [$this->entityManager, $data]);
+
+        self::assertInstanceOf("App\Entity\Quiz", $result);
+        self::assertEquals("Test Quiz Title", $result->getTitle());
+        self::assertEquals("test-quiz-title", $result->getSlug());
     }
 }
